e81c601faa4375590b4df1266d26fdd4ef9db774
Martin Chatterjee <martin@chatterjee.de>
2022-07-06 10:53:17 +0200

    Black and flake8 modifications.

--- python/houdini_ux.py
+++ python/houdini_ux.py
@@ -4,9 +4,6 @@
 # Licensed under MIT License (--> LICENSE)
 # -----------------------------------------------------------------------------
 
-import hou
-import toolutils
-
 """A collection of Houdini UX Helpers:
 
 - **toggleFullScreen**:
@@ -29,19 +26,23 @@ import toolutils
 
 - **pickwalking**  (up, down, left, right)
 
-
 """
+
+import hou
+import toolutils
+
 
 # -----------------------------------------------------------------------------
 def selectHierarchy():
-    """
-    """
-    selected = [node for node in hou.selectedNodes()
-                if node.type().category().name() == 'Object']
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
 
     def _selectRecursively(node):
-        """
-        """
+        """ """
         node.setSelected(True)
         for child in node.outputs():
             _selectRecursively(child)
@@ -52,30 +53,27 @@ def selectHierarchy():
 
 # -----------------------------------------------------------------------------
 def preparePickwalking():
-    """
-    """
-    selected = [node for node in hou.selectedNodes()
-                if node.type().category().name() == 'Object']
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
 
     for node in selected:
-        _ensureParm(node, 'pw_up', 'Up', folder_name='pickwalking')
-        _ensureParm(node, 'pw_right', 'Right', folder_name='pickwalking')
-        _ensureParm(node, 'pw_down', 'Down', folder_name='pickwalking')
-        _ensureParm(node, 'pw_left', 'Left', folder_name='pickwalking')
+        _ensureParm(node, "pw_up", "Up", folder_name="pickwalking")
+        _ensureParm(node, "pw_right", "Right", folder_name="pickwalking")
+        _ensureParm(node, "pw_down", "Down", folder_name="pickwalking")
+        _ensureParm(node, "pw_left", "Left", folder_name="pickwalking")
+
 
 # -----------------------------------------------------------------------------
-def _ensureParm(node,
-                parm_name,
-                label_name,
-                parm_start_value=None,
-                folder_name=None):
-    """
-    """
+def _ensureParm(node, parm_name, label_name, parm_start_value=None, folder_name=None):
+    """ """
     parm = node.parm(parm_name)
 
     if not parm:
-        folder = _ensureParmFolder(node,
-                                   folder_name=folder_name)
+        folder = _ensureParmFolder(node, folder_name=folder_name)
 
         ptg = node.parmTemplateGroup()
         parm_template = None
@@ -97,13 +95,13 @@ def _ensureParm(node,
 
 # -----------------------------------------------------------------------------
 def _ensureParmFolder(node, folder_name):
-    """
-    """
+    """ """
     ptg = node.parmTemplateGroup()
     folder = ptg.findFolder(folder_name)
     if not folder:
-        folder = hou.FolderParmTemplate(name='fld_{}'.format(folder_name.lower()),
-                                        label=folder_name)
+        folder = hou.FolderParmTemplate(
+            name="fld_{}".format(folder_name.lower()), label=folder_name
+        )
         ptg.append(folder)
         node.setParmTemplateGroup(ptg)
         ptg = node.parmTemplateGroup()
@@ -113,15 +111,17 @@ def _ensureParmFolder(node, folder_name):
 
 
 # -----------------------------------------------------------------------------
-def pickWalk(mode='up', replace=True):
-    """
-    """
-    selected = [node for node in hou.selectedNodes()
-                if node.type().category().name() == 'Object']
+def pickWalk(mode="up", replace=True):
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
     result = []
 
     for item in selected:
-        pickwalk = item.parm('pw_{}'.format(mode))
+        pickwalk = item.parm("pw_{}".format(mode))
         if pickwalk:
             # target_path = pickwalk.eval()
             target = item.node(pickwalk.eval())
@@ -138,15 +138,14 @@ def pickWalk(mode='up', replace=True):
 
 # -----------------------------------------------------------------------------
 def _walkHierarchy(item, mode, result, replace):
-    """
-    """
+    """ """
     target = None
 
-    if mode == 'up':
+    if mode == "up":
         parent = item.input(0)
         if parent:
             target = parent
-    elif mode == 'down':
+    elif mode == "down":
         outputs = item.outputs()
         if len(outputs):
             target = outputs[0]
@@ -155,9 +154,9 @@ def _walkHierarchy(item, mode, result, replace):
         if parent:
             siblings = parent.outputs()
             index = siblings.index(item)
-            if mode == 'right':
+            if mode == "right":
                 index = (index + 1) % len(siblings)
-            elif mode == 'left':
+            elif mode == "left":
                 index = (index - 1) % len(siblings)
             target = siblings[index]
 
@@ -166,12 +165,15 @@ def _walkHierarchy(item, mode, result, replace):
             item.setSelected(False)
         result.append(target)
 
+
 # -----------------------------------------------------------------------------
 def resetTransform():
-    """
-    """
-    selected = [node for node in hou.selectedNodes()
-                if node.type().category().name() == 'Object']
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
 
     identity = hou.Matrix4()
     identity.setToIdentity()
@@ -184,10 +186,12 @@ def resetTransform():
 
 # -----------------------------------------------------------------------------
 def parent():
-    """
-    """
-    selected = [node for node in hou.selectedNodes()
-                if node.type().category().name() == 'Object']
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
     if len(selected) < 2:
         return
 
@@ -195,7 +199,7 @@ def parent():
     children = selected[:-1]
 
     for child in children:
-        child.parm('keeppos').set(True)
+        child.parm("keeppos").set(True)
         child.setInput(0, parent)
 
     parent.setSelected(False)
@@ -203,21 +207,21 @@ def parent():
 
 # -----------------------------------------------------------------------------
 def unparent():
-    """
-    """
-    selected = [node for node in hou.selectedNodes()
-                if node.type().category().name() == 'Object']
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
 
     for node in selected:
-        node.parm('keeppos').set(True)
+        node.parm("keeppos").set(True)
         node.setInput(0, None)
-
 
 
 # -----------------------------------------------------------------------------
 def toggleFullScreen():
-    """Toggles the Full Screen state of the Houdini Main Window.
-    """
+    """Toggles the Full Screen state of the Houdini Main Window."""
     mw = hou.qt.mainWindow()
     if mw.isFullScreen():
         mw.showMaximized()
@@ -227,11 +231,12 @@ def toggleFullScreen():
 
 # -----------------------------------------------------------------------------
 def toggleObjectDisplay():
-    """Toggles the visibility flag of all selected Object-level Nodes.
-
-    """
-    objects = [item for item in hou.selectedNodes()
-               if item.type().category().name() == 'Object']
+    """Toggles the visibility flag of all selected Object-level Nodes."""
+    objects = [
+        item
+        for item in hou.selectedNodes()
+        if item.type().category().name() == "Object"
+    ]
 
     for node in objects:
         node.setDisplayFlag(not node.isDisplayFlagSet())
@@ -250,32 +255,35 @@ def isolateSelection():
     settings = viewport.settings()
 
     current_mask = settings.visibleObjects()
-    msg = ''
-    if current_mask != '*':
-        current_mask = '*'
-        msg = ''
+    msg = ""
+    if current_mask != "*":
+        current_mask = "*"
+        msg = ""
     else:
         isolate_me = []
-        selected_object_nodes = [item for item in hou.selectedNodes()
-                                 if item.type().category().name() == 'Object']
+        selected_object_nodes = [
+            item
+            for item in hou.selectedNodes()
+            if item.type().category().name() == "Object"
+        ]
         for node in selected_object_nodes:
             isolate_me.append(node.path())
-        current_mask = ' '.join(isolate_me)
-        msg = '\n'.join(isolate_me)
-        if msg != '':
-            msg = '\n\nISOLATED\n--------------\n' + msg
-        if current_mask == '':
-            current_mask = '*'
+        current_mask = " ".join(isolate_me)
+        msg = "\n".join(isolate_me)
+        if msg != "":
+            msg = "\n\nISOLATED\n--------------\n" + msg
+        if current_mask == "":
+            current_mask = "*"
 
     for viewport in scene_view.viewports():
         viewport.settings().setVisibleObjects(current_mask)
         viewport.draw()
 
-    flash_msg = 'Isolate Selection'
-    if msg == '':
-        flash_msg = 'Display All'
+    flash_msg = "Isolate Selection"
+    if msg == "":
+        flash_msg = "Display All"
     viewer = toolutils.sceneViewer()
-    viewer.flashMessage('houdini_ux.png', flash_msg, 1.0)
+    viewer.flashMessage("houdini_ux.png", flash_msg, 1.0)
     _ensureViewportMsg(msg)
 
 
@@ -291,9 +299,11 @@ def quickDisplay(slot):
     path = viewer.pwd().path()
 
     # get first selected node in path
-    selected_nodes = [item for item in hou.node(path).children()
-                      if item.type().category().name() != 'Object'
-                      and item.isSelected()]
+    selected_nodes = [
+        item
+        for item in hou.node(path).children()
+        if item.type().category().name() != "Object" and item.isSelected()
+    ]
     target = selected_nodes[0] if len(selected_nodes) else None
     if target:
         _storeQuickDisplayNode(path, slot, target)
@@ -303,13 +313,14 @@ def quickDisplay(slot):
         stored.setDisplayFlag(True)
         stored.setRenderFlag(True)
         stored.setSelected(False)
-        viewer.flashMessage('houdini_ux.png', 'QuickDisplay {}: {}'.format(slot, stored.name()), 1.0)
+        viewer.flashMessage(
+            "houdini_ux.png", "QuickDisplay {}: {}".format(slot, stored.name()), 1.0
+        )
 
 
 # -----------------------------------------------------------------------------
 def clearQuickDisplay(full=False):
-    """Clears the stored QuickDisplay nodes.
-    """
+    """Clears the stored QuickDisplay nodes."""
     if full:
         hou.session.quick_display_storage = {}
     else:
@@ -317,66 +328,66 @@ def clearQuickDisplay(full=False):
         path = viewer.pwd().path()
         storage = _getQuickDisplayStorage(path)
         storage.clear()
-        comment = 'QuickDisplay'
+        comment = "QuickDisplay"
         for node in hou.node(path).children():
             if node.comment().startswith(comment):
-                node.setComment('')
+                node.setComment("")
 
-    viewer.flashMessage('houdini_ux.png', 'QuickDisplay: cleared', 1.5)
+    viewer.flashMessage("houdini_ux.png", "QuickDisplay: cleared", 1.5)
 
 
 # -----------------------------------------------------------------------------
 def _ensureViewportMsg(msg):
-    """
-    """
-    cameras = hou.nodeType(hou.objNodeTypeCategory(), 'cam').instances()
+    """ """
+    cameras = hou.nodeType(hou.objNodeTypeCategory(), "cam").instances()
     for cam in cameras:
         ptg = cam.parmTemplateGroup()
-        folder = ptg.findFolder('Viewport Message')
+        folder = ptg.findFolder("Viewport Message")
         if not folder:
             folder = hou.FolderParmTemplate("folder", "Viewport Message")
             ptg.append(folder)
             cam.setParmTemplateGroup(ptg)
         param = None
         for item in folder.parmTemplates():
-            if item.name() == 'vcomment':
+            if item.name() == "vcomment":
                 param = item
                 break
         if not param:
-            param = ptg.appendToFolder(folder, hou.StringParmTemplate("vcomment", "vcomment", 1))
+            param = ptg.appendToFolder(
+                folder, hou.StringParmTemplate("vcomment", "vcomment", 1)
+            )
             cam.setParmTemplateGroup(ptg)
-        cam.parm('vcomment').set(msg)
+        cam.parm("vcomment").set(msg)
+
 
 # -----------------------------------------------------------------------------
 def _getQuickDisplayStorage(path):
-    """
-    """
-    if not hasattr(hou.session, 'quick_display_storage'):
+    """ """
+    if not hasattr(hou.session, "quick_display_storage"):
         hou.session.quick_display_storage = {}
     storage = hou.session.quick_display_storage
-    if not path in storage:
+    if path not in storage:
         storage[path] = {}
     return storage[path]
 
+
 # -----------------------------------------------------------------------------
 def _storeQuickDisplayNode(path, slot, node):
-    """
-    """
+    """ """
     storage = _getQuickDisplayStorage(path)
     storage[slot] = node.path()
 
-    comment = 'QuickDisplay: {}'.format(slot)
+    comment = "QuickDisplay: {}".format(slot)
     for child in hou.node(path).children():
         if child.comment() == comment:
-            child.setComment('')
-    node.setComment('QuickDisplay: {}'.format(slot))
-    node.setGenericFlag(hou.nodeFlag.DisplayComment,True)
+            child.setComment("")
+    node.setComment("QuickDisplay: {}".format(slot))
+    node.setGenericFlag(hou.nodeFlag.DisplayComment, True)
 
 
 # -----------------------------------------------------------------------------
 def _getStoredQuickDisplayNode(path, label):
-    """
-    """
+    """ """
     node = None
     storage = _getQuickDisplayStorage(path)
     stored_path = storage.get(label, None)
