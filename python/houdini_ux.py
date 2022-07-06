95e3afc2083e181fa1e9f307e647b454b583092b
Martin Chatterjee <martin@chatterjee.de>
2022-07-06 11:07:51 +0200

    Reorder functions.

--- python/houdini_ux.py
+++ python/houdini_ux.py
@@ -6,240 +6,33 @@
 
 """A collection of Houdini UX Helpers:
 
-- **toggleFullScreen**:
-  Toggles the Full Screen state of the Houdini Main Window.
-
-- **toggleObjectDisplay**:
-  Toggles the visibility of all selected Objects.
-
-- **IsolateSelection**:
+- **Isolate Selection**:
   Mimics the Maya/Softimage "Isolate Selection" workflow at the Objects level.
 
-- **QuickDisplay**:
+- **Quick Display**:
   Let's you store and load active/visible nodes in SOP's.
   This is intended to sort of mimic the UX behaviour of Nuke's viewer
   shortuts. (--> 1, 2, 3)
 
-- **parent** & **unparent** selected objects
+- **Toggle Object Display**:
+  Toggles the visibility of all selected objects.
 
-- **reset Transforms** for selected objects
+- **Parent** & **Unparent** selected objects based on selection.
 
-- **pickwalking**  (up, down, left, right)
+- **Reset Transforms** for selected objects.
+
+- **Select Hierarchy** for selected objects.
+
+- Setup **Pickwalking** for arbitrary objects.
+  (→ Move between them using the up/down/left/right keys)
+
+- **Toggle Full Screen**:
+  Toggles the Full Screen state of the Houdini Main Window.
 
 """
 
 import hou
 import toolutils
-
-
-# -----------------------------------------------------------------------------
-def selectHierarchy():
-    """ """
-    selected = [
-        node
-        for node in hou.selectedNodes()
-        if node.type().category().name() == "Object"
-    ]
-
-    def _selectRecursively(node):
-        """ """
-        node.setSelected(True)
-        for child in node.outputs():
-            _selectRecursively(child)
-
-    for node in selected:
-        _selectRecursively(node)
-
-
-# -----------------------------------------------------------------------------
-def preparePickwalking():
-    """ """
-    selected = [
-        node
-        for node in hou.selectedNodes()
-        if node.type().category().name() == "Object"
-    ]
-
-    for node in selected:
-        _ensureParm(node, "pw_up", "Up", folder_name="pickwalking")
-        _ensureParm(node, "pw_right", "Right", folder_name="pickwalking")
-        _ensureParm(node, "pw_down", "Down", folder_name="pickwalking")
-        _ensureParm(node, "pw_left", "Left", folder_name="pickwalking")
-
-
-# -----------------------------------------------------------------------------
-def _ensureParm(node, parm_name, label_name, parm_start_value=None, folder_name=None):
-    """ """
-    parm = node.parm(parm_name)
-
-    if not parm:
-        folder = _ensureParmFolder(node, folder_name=folder_name)
-
-        ptg = node.parmTemplateGroup()
-        parm_template = None
-
-        parm_template = hou.StringParmTemplate(parm_name, label_name, 1)
-        parm_template.setStringType(hou.stringParmType.NodeReference)
-        if parm_start_value:
-            parm_template.setDefaultValue((parm_start_value,))
-
-        if parm_template:
-            ptg.appendToFolder(folder, parm_template)
-            node.setParmTemplateGroup(ptg)
-            parm = node.parm(parm_name)
-            if parm_start_value:
-                parm.set(parm_start_value)
-
-    return parm
-
-
-# -----------------------------------------------------------------------------
-def _ensureParmFolder(node, folder_name):
-    """ """
-    ptg = node.parmTemplateGroup()
-    folder = ptg.findFolder(folder_name)
-    if not folder:
-        folder = hou.FolderParmTemplate(
-            name="fld_{}".format(folder_name.lower()), label=folder_name
-        )
-        ptg.append(folder)
-        node.setParmTemplateGroup(ptg)
-        ptg = node.parmTemplateGroup()
-        folder = ptg.findFolder(folder_name)
-
-    return folder
-
-
-# -----------------------------------------------------------------------------
-def pickWalk(mode="up", replace=True):
-    """ """
-    selected = [
-        node
-        for node in hou.selectedNodes()
-        if node.type().category().name() == "Object"
-    ]
-    result = []
-
-    for item in selected:
-        pickwalk = item.parm("pw_{}".format(mode))
-        if pickwalk:
-            # target_path = pickwalk.eval()
-            target = item.node(pickwalk.eval())
-            if target:
-                result.append(target)
-                if replace:
-                    item.setSelected(False)
-        else:
-            _walkHierarchy(item, mode, result, replace)
-
-    for item in result:
-        item.setSelected(True, show_asset_if_selected=True)
-
-
-# -----------------------------------------------------------------------------
-def _walkHierarchy(item, mode, result, replace):
-    """ """
-    target = None
-
-    if mode == "up":
-        parent = item.input(0)
-        if parent:
-            target = parent
-    elif mode == "down":
-        outputs = item.outputs()
-        if len(outputs):
-            target = outputs[0]
-    else:
-        parent = item.input(0)
-        if parent:
-            siblings = parent.outputs()
-            index = siblings.index(item)
-            if mode == "right":
-                index = (index + 1) % len(siblings)
-            elif mode == "left":
-                index = (index - 1) % len(siblings)
-            target = siblings[index]
-
-    if target:
-        if replace:
-            item.setSelected(False)
-        result.append(target)
-
-
-# -----------------------------------------------------------------------------
-def resetTransform():
-    """ """
-    selected = [
-        node
-        for node in hou.selectedNodes()
-        if node.type().category().name() == "Object"
-    ]
-
-    identity = hou.Matrix4()
-    identity.setToIdentity()
-
-    for item in selected:
-        item.setParmTransform(identity)
-        item.setPreTransform(identity)
-        item.setParmPivotTransform(identity)
-
-
-# -----------------------------------------------------------------------------
-def parent():
-    """ """
-    selected = [
-        node
-        for node in hou.selectedNodes()
-        if node.type().category().name() == "Object"
-    ]
-    if len(selected) < 2:
-        return
-
-    parent = selected[-1]
-    children = selected[:-1]
-
-    for child in children:
-        child.parm("keeppos").set(True)
-        child.setInput(0, parent)
-
-    parent.setSelected(False)
-
-
-# -----------------------------------------------------------------------------
-def unparent():
-    """ """
-    selected = [
-        node
-        for node in hou.selectedNodes()
-        if node.type().category().name() == "Object"
-    ]
-
-    for node in selected:
-        node.parm("keeppos").set(True)
-        node.setInput(0, None)
-
-
-# -----------------------------------------------------------------------------
-def toggleFullScreen():
-    """Toggles the Full Screen state of the Houdini Main Window."""
-    mw = hou.qt.mainWindow()
-    if mw.isFullScreen():
-        mw.showMaximized()
-    else:
-        mw.showFullScreen()
-
-
-# -----------------------------------------------------------------------------
-def toggleObjectDisplay():
-    """Toggles the visibility flag of all selected Object-level Nodes."""
-    objects = [
-        item
-        for item in hou.selectedNodes()
-        if item.type().category().name() == "Object"
-    ]
-
-    for node in objects:
-        node.setDisplayFlag(not node.isDisplayFlagSet())
 
 
 # -----------------------------------------------------------------------------
@@ -334,6 +127,216 @@ def clearQuickDisplay(full=False):
                 node.setComment("")
 
     viewer.flashMessage("houdini_ux.png", "QuickDisplay: cleared", 1.5)
+
+
+# -----------------------------------------------------------------------------
+def toggleObjectDisplay():
+    """Toggles the visibility flag of all selected Object-level Nodes."""
+    objects = [
+        item
+        for item in hou.selectedNodes()
+        if item.type().category().name() == "Object"
+    ]
+
+    for node in objects:
+        node.setDisplayFlag(not node.isDisplayFlagSet())
+
+
+# -----------------------------------------------------------------------------
+def parent():
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
+    if len(selected) < 2:
+        return
+
+    parent = selected[-1]
+    children = selected[:-1]
+
+    for child in children:
+        child.parm("keeppos").set(True)
+        child.setInput(0, parent)
+
+    parent.setSelected(False)
+
+
+# -----------------------------------------------------------------------------
+def unparent():
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
+
+    for node in selected:
+        node.parm("keeppos").set(True)
+        node.setInput(0, None)
+
+
+# -----------------------------------------------------------------------------
+def resetTransform():
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
+
+    identity = hou.Matrix4()
+    identity.setToIdentity()
+
+    for item in selected:
+        item.setParmTransform(identity)
+        item.setPreTransform(identity)
+        item.setParmPivotTransform(identity)
+
+
+# -----------------------------------------------------------------------------
+def selectHierarchy():
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
+
+    def _selectRecursively(node):
+        """ """
+        node.setSelected(True)
+        for child in node.outputs():
+            _selectRecursively(child)
+
+    for node in selected:
+        _selectRecursively(node)
+
+
+# -----------------------------------------------------------------------------
+def preparePickwalking():
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
+
+    for node in selected:
+        _ensureParm(node, "pw_up", "Up", folder_name="pickwalking")
+        _ensureParm(node, "pw_right", "Right", folder_name="pickwalking")
+        _ensureParm(node, "pw_down", "Down", folder_name="pickwalking")
+        _ensureParm(node, "pw_left", "Left", folder_name="pickwalking")
+
+
+# -----------------------------------------------------------------------------
+def pickWalk(mode="up", replace=True):
+    """ """
+    selected = [
+        node
+        for node in hou.selectedNodes()
+        if node.type().category().name() == "Object"
+    ]
+    result = []
+
+    for item in selected:
+        pickwalk = item.parm("pw_{}".format(mode))
+        if pickwalk:
+            # target_path = pickwalk.eval()
+            target = item.node(pickwalk.eval())
+            if target:
+                result.append(target)
+                if replace:
+                    item.setSelected(False)
+        else:
+            _walkHierarchy(item, mode, result, replace)
+
+    for item in result:
+        item.setSelected(True, show_asset_if_selected=True)
+
+
+# -----------------------------------------------------------------------------
+def toggleFullScreen():
+    """Toggles the Full Screen state of the Houdini Main Window."""
+    mw = hou.qt.mainWindow()
+    if mw.isFullScreen():
+        mw.showMaximized()
+    else:
+        mw.showFullScreen()
+
+
+# -----------------------------------------------------------------------------
+def _ensureParm(node, parm_name, label_name, parm_start_value=None, folder_name=None):
+    """ """
+    parm = node.parm(parm_name)
+
+    if not parm:
+        folder = _ensureParmFolder(node, folder_name=folder_name)
+
+        ptg = node.parmTemplateGroup()
+        parm_template = None
+
+        parm_template = hou.StringParmTemplate(parm_name, label_name, 1)
+        parm_template.setStringType(hou.stringParmType.NodeReference)
+        if parm_start_value:
+            parm_template.setDefaultValue((parm_start_value,))
+
+        if parm_template:
+            ptg.appendToFolder(folder, parm_template)
+            node.setParmTemplateGroup(ptg)
+            parm = node.parm(parm_name)
+            if parm_start_value:
+                parm.set(parm_start_value)
+
+    return parm
+
+
+# -----------------------------------------------------------------------------
+def _ensureParmFolder(node, folder_name):
+    """ """
+    ptg = node.parmTemplateGroup()
+    folder = ptg.findFolder(folder_name)
+    if not folder:
+        folder = hou.FolderParmTemplate(
+            name="fld_{}".format(folder_name.lower()), label=folder_name
+        )
+        ptg.append(folder)
+        node.setParmTemplateGroup(ptg)
+        ptg = node.parmTemplateGroup()
+        folder = ptg.findFolder(folder_name)
+
+    return folder
+
+
+# -----------------------------------------------------------------------------
+def _walkHierarchy(item, mode, result, replace):
+    """ """
+    target = None
+
+    if mode == "up":
+        parent = item.input(0)
+        if parent:
+            target = parent
+    elif mode == "down":
+        outputs = item.outputs()
+        if len(outputs):
+            target = outputs[0]
+    else:
+        parent = item.input(0)
+        if parent:
+            siblings = parent.outputs()
+            index = siblings.index(item)
+            if mode == "right":
+                index = (index + 1) % len(siblings)
+            elif mode == "left":
+                index = (index - 1) % len(siblings)
+            target = siblings[index]
+
+    if target:
+        if replace:
+            item.setSelected(False)
+        result.append(target)
 
 
 # -----------------------------------------------------------------------------
